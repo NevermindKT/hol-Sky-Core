55ec8ecc458e1f7c8d9694dda5b8617ca2c6d508
Vladyslav Krykun <Kriku_sq78@student.itstep.org>
2026-08-07 14:49:06 +0300

    Start of the integration of the rain to the road

--- scripts/weather/rain_controller.gd
+++ scripts/weather/rain_controller.gd
@@ -5,7 +5,7 @@ extends Node
 
 func _ready():
 	WeatherManager.weather_changed.connect(_on_weather_changed)
-	
+
 	if WeatherManager.weather_data && WeatherManager.weather_data.rain:
 		apply_data(WeatherManager.weather_data.rain)
 
@@ -17,13 +17,13 @@ func _on_weather_changed():
 
 func apply_data(data: RainData):
 	rain_particles.amount = data.amount
-	
+
 	var mesh = rain_particles.draw_pass_1 as QuadMesh
 	if mesh:
 		mesh.size = Vector2(data.drop_width, data.drop_length)
-		
+
 	var shader = rain_particles.material_override as ShaderMaterial
 	if shader:
 		shader.set_shader_parameter("rain_color", data.rain_color)
-		
+
 	rain_particles.emitting = true
