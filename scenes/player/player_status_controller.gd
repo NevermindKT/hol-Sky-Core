4ea6662cf98febd21a93865ca28b7a6803857918
NVKT <nevermindktab@gmail.com>
2026-08-17 16:24:55 +0300

    health last chance system

--- scenes/player/player_status_controller.gd
+++ scenes/player/player_status_controller.gd
@@ -4,10 +4,9 @@ class_name Player_Status_Controller
 var current_health: float
 @export var max_health: float
 
-
 func initialize() -> void:
 	current_health = max_health
-
+	
 	Events.player_heal.connect(heal)
 	Events.player_take_damage.connect(take_damage)
 
@@ -23,6 +22,13 @@ func heal(heal_value: float) -> void:
 
 func take_damage(damage_value: float) -> void:
 	print("Player taked damage!")
+
+	if current_health > max_health * 0.1:
+		if current_health - damage_value <= 0.0:
+			one_shot_protect()
+			Events.player_health_changed.emit(current_health)
+			return
+
 	current_health -= damage_value
 	clamp_health()
 
@@ -43,3 +49,7 @@ func is_dead() -> bool:
 func death() -> void:
 	print("player is dead!")
 	Events.player_died.emit()
+
+
+func one_shot_protect() -> void:
+	current_health = max_health * 0.01
