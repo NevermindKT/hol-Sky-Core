12edcbff0f76d9547dbe701415338b2c4dd7fa72
NVKT <nevermindktab@gmail.com>
2026-09-24 17:03:46 +0300

    bolt fix

--- turret_move.gd
+++ turret_move.gd
@@ -11,10 +11,12 @@ class_name GunMove
 @export var gun_point: Marker3D
 @export var muzzle_flash: Muzzle_flash
 
+var bolt_offset: Transform3D
 
 func _ready() -> void:
 	gun.override_pose = true
 	bolt.override_pose = true
+	bolt_offset = gun.global_transform.affine_inverse() * bolt.global_transform
 
 
 func aim_pitch(local_target: Vector3, delta: float) -> void:
@@ -23,6 +25,7 @@ func aim_pitch(local_target: Vector3, delta: float) -> void:
 	desired_pitch = clamp(desired_pitch, deg_to_rad(min_pitch_deg), deg_to_rad(max_pitch_deg))
 
 	gun.rotation.x = move_toward(gun.rotation.x, desired_pitch, pitch_speed * delta)
+	bolt.global_transform = gun.global_transform * bolt_offset
 
 
 func play_bolt_recoil(travel: float = 0.05, out_time: float = 0.04, in_time: float = 0.12) -> void:
