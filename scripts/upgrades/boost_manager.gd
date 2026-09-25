b373fc89cea9c64dd3e9f3b95550b02a5405b927
Vladyslav Krykun <mr.laggermpc.890@gmail.com>
2026-09-25 13:51:00 +0200

    Nitro speed lines

--- scripts/upgrades/boost_manager.gd
+++ scripts/upgrades/boost_manager.gd
@@ -1,6 +1,6 @@
 extends Node
 
-const TEST_BOOST_PATH := "res://resources/upgrades/boosts/flashbang.tres"
+const TEST_BOOST_PATH := "res://resources/upgrades/boosts/nitro.tres"
 
 var owned: Dictionary = {}
 var active: Array[BoostData] = []
